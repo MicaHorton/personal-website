90b0c4f71cf496636141275f29ce60094bd456dd
Mica Horton <mica.e.horton@gmail.com>
2021-05-03 15:24:11 -0700

    Fix npm warnings about unused imports

--- frontend/src/components/single.js
+++ frontend/src/components/single.js
@@ -1,36 +1,4 @@
 import styles from '../styles/single.module.css';
-import React, { useEffect, useState } from 'react';
-import { getSinglePost } from '../api.js';
-import { useParams } from 'react-router-dom';
-
-/*
-const Single = props => {
-    const [post, setPost] = useState([]);
-    let { id } = useParams();
-
-    useEffect(() => {
-        getSinglePost(id)
-        .then(post => {
-            setPost(post);
-        })
-        .catch(err => console.log(err))
-
-    }, []);
-
-    if (post) {
-        return (
-            <main className={styles.single}>
-                <div className={styles.box} 
-                dangerouslySetInnerHTML={{ __html: post.sanatizedHtml }} />
-            </main>
-        );
-    }
-
-    return null;
-}
-
-export default Single;*/
-
 
 const Single = props => {
     if (props.post) {
