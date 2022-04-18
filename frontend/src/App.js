35df099b7efbda12cb07dd556e20c3afc7111c83
Mica Horton <mica.e.horton@gmail.com>
2022-04-18 15:52:40 -0700

    Create placeholder message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar.js";
 import About from "./components/about.js";
 import Projects from "./components/projects.js";
 import Blog from "./components/blog.js";
-import Single from './components/single.js';
-import Contact from './components/contact.js';
-import { getAllPosts } from './api.js';
-
+import Single from "./components/single.js";
+import Contact from "./components/contact.js";
+import { getAllPosts } from "./api.js";
 
 const App = () => {
-    const [posts, setPosts] = useState([]);
-    
-    useEffect(() => {
-        getAllPosts()
-        .then(posts => {
-            setPosts(posts);
-        })
-        .catch(err => console.log(err))
+  const [posts, setPosts] = useState([]);
 
-    }, []);
+  useEffect(() => {
+    getAllPosts()
+      .then((posts) => {
+        setPosts(posts);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <BrowserRouter>
-            <Navbar exact path='/' />
-            <Route exact path='/' component={About} />
-            <Route exact path='/projects' component={Projects} />
-            <Route exact path='/blog'>
-                <Blog posts={posts}/>
-            </Route>
-            <Route exact path='/blog/:id' render={(props) => {
-                let id = props.match.params.id;
-                let post = posts.filter(x => x._id === id)[0];
-                return <Single post={post}/>;
-            }}/>
-            <Route exact path='/contact' component={Contact} />
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/blog">
+        <Blog posts={posts} />
+      </Route>
+      <Route
+        exact
+        path="/blog/:id"
+        render={(props) => {
+          let id = props.match.params.id;
+          let post = posts.filter((x) => x._id === id)[0];
+          return <Single post={post} />;
+        }}
+      />
+      <Route exact path="/contact" component={Contact} />
+    </BrowserRouter>
+  );
+};
 
 export default App;
-
 
 /*
 <Route exact path='/blog/:id' component={Single}/>
--- frontend/src/components/about.js
+++ frontend/src/components/about.js
@@ -1,76 +1,26 @@
-import 'font-awesome/css/font-awesome.min.css';
+import "font-awesome/css/font-awesome.min.css";
 
 const About = () => (
-    <main>
-        <section>
-            <h1>Hi, my name is Mica Horton</h1>
-            <p>I'm an LAHS student living in the Bay Area who loves to code. 
-            This website is a tribute to all the projects I've done. 
-            Check out my <a href='https://www.micahorton.com/projects'>projects</a>
-            /<a href='https://www.micahorton.com/blog'>blog</a> or learn more about me at:</p>
-
-            <ul className="icons">
-                <li>
-                    <i className="fa fa-github"></i>
-                    <a href="https://github.com/MicaHorton" target="_blank" rel="noreferrer">GitHub</a>
-                </li>
-                <li>
-                    <i className="fa fa-stack-overflow"></i>
-                    <a href="https://stackoverflow.com/users/13029356/mica-horton?tab=profile" 
-                    target="_blank" rel="noreferrer">Stack Overflow</a>
-                </li>
-                <li>
-                    <i className="fa fa-linkedin-square"></i>
-                    <a href="https://www.linkedin.com/in/micahorton/" 
-                    target="_blank" rel="noreferrer">LinkedIn</a>
-                </li>
-            </ul>
-        </section>
-        
-        
-        <section>
-            <h1>Languages I Know</h1>
-            <p>In order of most experience to least.</p>
-            <ul>
-                <li>HTML, CSS, JavaScript (with the React framework)</li>
-                <li>Python</li>
-                <li>Node.js</li>
-                <li>C++ (mainly in the context of Arduino)</li>
-                <li>Pascal</li>
-            </ul>
-        </section>
-
-        <section>
-            <h1>Events I've Gone To</h1>
-            <ul>
-                <li>LAHS Hacks IV</li>
-                <li>Superposition Hacks IV (<a href='https://github.com/MicaHorton/hqrecycle' target='_blank' rel="noreferrer" className='italicLink'>see project</a>)</li>
-                <li>Bay Area Science Fesitval</li>
-                <li>NIMBLE Youth Innovation Showcase</li>
-            </ul>
-        </section>
-
-        <section>
-            <h1>Other Acheivements</h1>
-            <ul>
-                <li> President of Adventure Club, the hiking and camping club focused on 
-                    going outside even though staying home and programming is fun too. 
-                    (See our Instagram @lahsadventureclub.) </li>
-                <li> Passed John Hopkins Center of Talented Youth Assessment </li>
-                <li> Received Challenger Merit Scholarship </li>
-                <li> Studied Spanish for 4 years, currently enrolled in SPAN 5. 
-                    Received the silver metal for the NHS Spanish exam. </li>
-                <li> Has completed 20 service hours volunteering at the local TV station 
-                    (the KMVT) as a cinematographer. </li>
-            </ul>
-        </section>
-
-      </main>
-
+  <main>
+    <section>
+      <h1>Hi, there!</h1>
+      <p>
+        Unfortunately, this website has become too expensive to host, so I've
+        taken it down temporarily. I plan to put it back up in the future
+        sometime when my budget allows :/ The original code and screenshots of
+        how the website looked before can be seen on my Github at
+        github.com/MicaHorton.
+        <br />
+        <br />
+        Cheers,
+        <br />
+        Mica
+      </p>
+    </section>
+  </main>
 );
 
 export default About;
-
 
 /*
  <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
